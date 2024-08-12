4477f42c460dd19ccd25a395e61dd862fe6502df
Garret Alegre <130976170+gartgetget@users.noreply.github.com>
2024-08-12 13:24:58 +0800

    Files Upload

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('mobile-open-button').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
